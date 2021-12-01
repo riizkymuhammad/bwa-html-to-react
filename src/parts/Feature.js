5b37a8eb941b3047e3f55ccd06df1431923b1984
Muhammad Rizky <mrizky98@gmail.com>
2021-12-01 13:35:34 +0700

    Clients

---
+++ src/parts/Feature.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+import propTypes from 'prop-Types';
+
+export default function Feature(props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import HeroImage from 'assets/images/hero-image.png'
+import Fade from 'react-reveal/Fade';
 
 import Section from 'elements/Section';
 import Header from "parts/Header";
+import Clients from "parts/Clients";
 
 export default function Homepage(){
     return(
@@ -13,39 +15,39 @@ export default function Homepage(){
                   	<div className="container-sm">
 						<div className="hero-inner section-inner">
 							<div className="hero-content">
-								<h1
-									className="mt-0 mb-16 reveal-from-bottom"
-									data-reveal-delay="200"
-								>
+								<Fade bottom delay = {500}>
+								<h1 className="mt-0 mb-16 reveal-from-bottom">
 									Landing template for startups
-								</h1>
+									</h1>
+								</Fade>
+								
 								<div className="container-xs">
-									<p
-										className="mt-0 mb-32 reveal-from-bottom"
-										data-reveal-delay="400"
-									>
+									<Fade bottom delay = {1000}>
+										<p className="mt-0 mb-32 reveal-from-bottom">
 										Our landing page template works on all
 										devices, so you only have to set it up
 										once, and get beautiful results forever.
-									</p>
+										</p>
+									</Fade>
+									
 								</div>
 							</div>
-							<div
-								className="hero-figure reveal-from-bottom illustration-element-01"
-								data-reveal-value="20px"
-								data-reveal-delay="800"
-							>
-								<img
+							<div className="hero-figure reveal-from-bottom illustration-element-01">
+								<Fade bottom delay={1500}>
+									<img
 									className="has-shadow"
 									src={HeroImage}
 									alt="Hero image"
 									width="896"
 									height="504"
 								/>
+								</Fade>
+								
 							</div>
 						</div>
 					</div>
-              </Section>
+				</Section>
+				<Clients></Clients>
             </main>
         </div>
     
